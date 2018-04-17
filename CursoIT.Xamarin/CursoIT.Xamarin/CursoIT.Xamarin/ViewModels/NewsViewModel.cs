817972d11e52408c22ba6ea8f73385333c8fdb4b
andreslon <andreslon@outlook.com>
2018-04-16 20:21:18 -0500

    Mvvm Clase 3

---
+++ CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin/ViewModels/NewsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoIT.Xamarin.ViewModels
+{
+    class NewsViewModel : INotifyPropertyChanged
+    {
+        public NewsViewModel()
+        {
+            this.Name = "Andres";
+            LoadData();
+        }
+
+        async private void LoadData()
+        {
+            IsLoading = true;
+            await Task.Delay(5000);
+            IsLoading = false;
+        }
+
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        private bool _IsLoading;
+        public bool IsLoading
+        {
+            get { return _IsLoading; }
+            set
+            {
+                _IsLoading = value;
+                OnPropertyChanged("IsLoading");
+            }
+        }
+        private string _Description;
+        public string Description
+        {
+            get { return _Description; }
+            set
+            {
+                _Description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+
+
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
