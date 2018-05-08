c7dd2b8bd5d778a087f0ba815c60982b8d508dc0
andreslon <andreslon@outlook.com>
2018-05-07 21:31:14 -0500

    Clase 5

--- CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin.Android/MainActivity.cs
+++ CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
 
 namespace CursoIT.Xamarin.Droid
 {
@@ -18,7 +19,7 @@ namespace CursoIT.Xamarin.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+            CachedImageRenderer.Init(true);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
