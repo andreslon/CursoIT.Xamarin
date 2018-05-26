b4787d39276e1b2ac5c2b84c7133f91491bf2dc9
andreslon <andreslon@outlook.com>
2018-05-26 11:35:08 -0500

    Implementación de splash

--- CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin.Android/Resources/Resource.designer.cs
+++ CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin.Android/Resources/Resource.designer.cs
@@ -2192,26 +2192,26 @@ namespace CursoIT.Xamarin.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_1 = 2130837778;
-			
 			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_2 = 2130837779;
+			public const int avd_hide_password_1 = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_3 = 2130837780;
+			public const int avd_hide_password_2 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_hide_password_3 = 2130837781;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_1 = 2130837781;
-			
 			// aapt resource value: 0x7f020116
-			public const int avd_show_password_2 = 2130837782;
+			public const int avd_show_password_1 = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_3 = 2130837783;
+			public const int avd_show_password_2 = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int avd_show_password_3 = 2130837784;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2765,11 +2765,11 @@ namespace CursoIT.Xamarin.Droid
 			// aapt resource value: 0x7f02010c
 			public const int notification_icon_background = 2130837772;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_bg = 2130837776;
-			
 			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_low_bg = 2130837777;
+			public const int notification_template_icon_bg = 2130837777;
+			
+			// aapt resource value: 0x7f020112
+			public const int notification_template_icon_low_bg = 2130837778;
 			
 			// aapt resource value: 0x7f02010d
 			public const int notification_tile_bg = 2130837773;
@@ -2778,7 +2778,10 @@ namespace CursoIT.Xamarin.Droid
 			public const int notify_panel_notification_icon_bg = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
-			public const int splash_screen = 2130837775;
+			public const int splash = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int splash_screen = 2130837776;
 			
 			static Drawable()
 			{
@@ -4333,6 +4336,9 @@ namespace CursoIT.Xamarin.Droid
 			
 			// aapt resource value: 0x7f0b018a
 			public const int MainTheme_Base = 2131427722;
+			
+			// aapt resource value: 0x7f0b018c
+			public const int MainTheme_Splash = 2131427724;
 			
 			// aapt resource value: 0x7f0b002d
 			public const int Platform_AppCompat = 2131427373;
---
+++ CursoIT.Xamarin/CursoIT.Xamarin/CursoIT.Xamarin.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CursoIT.Xamarin.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory =true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        async private void Startup()
+        {
+            await Task.Delay(1000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
